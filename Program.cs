40295d6568701c1d0e5ac3cda213f64ad3cea5e3
yeah124 <patrik.fejes@gmail.com>
2024-10-14 14:32:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace osztaly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = 4.6;
+            Circle c = new Circle(r);
+            c.printA();
+            c.printP();
+        }
+    }
+}
