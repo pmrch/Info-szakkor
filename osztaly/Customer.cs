c8957749fd102922fa54a0ffdb0b50b38eff3164
yeah124 <patrik.fejes@gmail.com>
2024-10-22 18:35:00 +0200

    Nice

---
+++ osztaly/Customer.cs
@@ -0,0 +1,22 @@
+﻿namespace osztaly
+{
+    internal class Customer
+    {
+        static void Main(string[] args)
+        {
+
+            Customer_CLASS c = new Customer_CLASS(10, "PAUL", "LONDON");
+            
+            // 10-elemű tömb Customer_CLASS típusban
+            Customer_CLASS[] tomb = new Customer_CLASS[10];
+            
+            // Már nem is tudom
+            List<Customer_CLASS> list = new List<Customer_CLASS>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(new Customer_CLASS(i, "PETER", "SZEGED"));
+            }
+        }
+    }
+}
---
+++ osztaly/Program_osztaly.cs
@@ -0,0 +1,14 @@
+﻿namespace osztaly
+{
+    internal class Program_osztaly
+    {
+        static void Main(string[] args)
+        {
+            double r = 4.6;
+            Circle c = new Circle(r);
+            
+            c.printA();
+            c.printP();
+        }
+    }
+}
